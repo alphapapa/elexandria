69502d7f23b18a93ce4b6f8c1e698c85590c8384
Adam Porter <adam@alphapapa.net>
2017-11-20 07:50:25 -0600

    Initial commit

---
+++ elexandria.el
@@ -0,0 +1,71 @@
+;;; Code:
+
+;;;; Requirements
+
+(require 'dash)
+
+;;;; Macros
+
+;;;;; EIEIO
+
+(defmacro oref* (&rest slots)
+  "Access SLOTS of nested EIEIO objects.
+The first of SLOTS should be an object, while the rest should be
+slot symbols.  Accessing each slot should return an object for
+which the next slot is valid, except for the last slot, which may
+return any value."
+  (cl-labels ((rec (slots)
+                   `(oref ,(if (and (consp (cdr slots))
+                                    (cddr slots))
+                               (rec (cdr slots))
+                             (cadr slots))
+                          ,(car slots))))
+    (rec (nreverse slots))))
+
+(defmacro with-slots* (slots-objects &rest body)
+  "Access slots of nested objects, evaluating BODY.
+Creates nested `with-slots' forms, so each slot is a generalized
+variable.  For example:
+
+\(with-slots* (((id session) room)
+              ((user) session))
+             user)
+
+Is transformed to:
+
+\(with-slots (id session) room
+  (with-slots (user) session
+    user))"
+  (declare (indent defun))
+  (cl-loop for (slots object) in (reverse slots-objects)
+           do (setq body `((with-slots ,slots ,object ,@body)))
+           finally return (car body)))
+
+(defmacro defclass* (name superclasses slots &rest options-and-doc)
+  "Like `defclass', but supports instance initforms.
+Each slot may have an `:instance-initform', which is evaluated in
+the context of the object's slots when each instance is
+initialized."
+  (declare (indent defun))
+  (let* ((slot-inits (-non-nil (--map (let ((name (car it))
+                                            (initer (plist-get (cdr it) :instance-initform)))
+                                        (when initer
+                                          (list 'setq name initer)))
+                                      slots)))
+         (slot-names (mapcar #'car slots))
+         (around-fn-name (intern (concat (symbol-name name) "-initialize")))
+         (docstring (format "Inititalize instance of %s." name)))
+    `(progn
+       (defclass ,name ,superclasses ,slots ,@options-and-doc)
+       (when (> (length ',slot-inits) 0)
+         (cl-defmethod initialize-instance :after ((this ,name) &rest _)
+                       ,docstring
+                       (with-slots ,slot-names this
+                         ,@slot-inits))))))
+
+
+;;;; Footer
+
+(provide 'elexandria)
+
+;;; elexandria.el ends here
